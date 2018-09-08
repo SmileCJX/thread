99d9a2771100e868988f021ce6fd233a4c299568
SmileCJX <1092925421@qq.com>
2018-09-08 15:18:33 +0800

    守护线程

--- src/pers/caijx/thread/t2/Demo1.java
+++ src/pers/caijx/thread/t2/Demo1.java
@@ -11,13 +11,25 @@ public class Demo1 extends Thread {
 
     @Override
     public void run() {
-        System.out.println(getName() + "线程执行了..");
+        while (true) {
+            System.out.println(getName() + "线程执行了..");
+        }
     }
 
     public static void main(String[] args) {
         Demo1 d1 = new Demo1("first-thread");
         Demo1 d2 = new Demo1("second-thread");
+
+        d1.setDaemon(true); // 守护线程，主线程退出后，无论是否结束均停止
+        d2.setDaemon(true);
+
         d1.start();
         d2.start();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
