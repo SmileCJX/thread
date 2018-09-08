51e62c53c311c521a98a48180ffc50f2d80cc248
SmileCJX <1092925421@qq.com>
2018-09-09 00:45:20 +0800

    使用定时器完成线程

---
+++ src/pers/caijx/thread/t2/Demo5.java
@@ -0,0 +1,21 @@
+package pers.caijx.thread.t2;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by caijx on 2018/9/9/009.
+ */
+public class Demo5 {
+
+    public static void main(String[] args) {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                // 实现定时任务
+                System.out.println("timetask is run");
+            }
+        },0,1000);
+    }
+}
