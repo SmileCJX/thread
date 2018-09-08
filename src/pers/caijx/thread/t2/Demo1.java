a9dfec28d302667122273ad2da24b2202ccdf568
SmileCJX <1092925421@qq.com>
2018-09-08 15:28:23 +0800

    正确的中断方式

--- src/pers/caijx/thread/t2/Demo1.java
+++ src/pers/caijx/thread/t2/Demo1.java
@@ -11,8 +11,13 @@ public class Demo1 extends Thread {
 
     @Override
     public void run() {
-        while (true) {
+        while (!interrupted()) {
             System.out.println(getName() + "线程执行了..");
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -20,16 +25,17 @@ public class Demo1 extends Thread {
         Demo1 d1 = new Demo1("first-thread");
         Demo1 d2 = new Demo1("second-thread");
 
-        d1.setDaemon(true); // 守护线程，主线程退出后，无论是否结束均停止
-        d2.setDaemon(true);
+//        d1.setDaemon(true); // 守护线程，主线程退出后，无论是否结束均停止
+//        d2.setDaemon(true);
 
         d1.start();
         d2.start();
 
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        d1.interrupt();
+//        try {
+//            Thread.sleep(2000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
     }
 }
