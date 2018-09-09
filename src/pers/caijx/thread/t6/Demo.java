5872b738ce7dbd72592e694171ad87919e1c5f2a
SmileCJX <1092925421@qq.com>
2018-09-09 20:24:25 +0800

    重入锁

--- src/pers/caijx/thread/t6/Demo.java
+++ src/pers/caijx/thread/t6/Demo.java
@@ -8,19 +8,38 @@ public class Demo {
 
     public synchronized void a() {
         System.out.println("a");
-        b();
+//        b();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public synchronized void b() {
         System.out.println("b");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
+        Demo d = new Demo();  //使用相同对象，锁重入
+//        Demo d1 = new Demo();
+//        Demo d2 = new Demo();
         new Thread(new Runnable() {
             @Override
             public void run() {
-                Demo d = new Demo();
                 d.a();
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                d.b();
             }
         }).start();
     }
