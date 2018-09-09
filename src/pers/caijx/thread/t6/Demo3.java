9b793b6f435d40c2b71b4bb697c8de723438bd2f
SmileCJX <1092925421@qq.com>
2018-09-09 20:58:12 +0800

    死锁

---
+++ src/pers/caijx/thread/t6/Demo3.java
@@ -0,0 +1,54 @@
+package pers.caijx.thread.t6;
+
+/**
+ * Created by caijx on 2018/9/9/009.
+ */
+public class Demo3 {
+
+    private Object obj1 = new Object();
+    private Object obj2 = new Object();
+
+    public void a() {
+        synchronized (obj1) {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            synchronized (obj2) {
+                System.out.println("a");
+            }
+        }
+    }
+
+    public void b() {
+        synchronized (obj2) {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            synchronized (obj1) {
+                System.out.println("b");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Demo3 d = new Demo3();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                d.a();
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                d.b();
+            }
+        }).start();
+    }
+}
