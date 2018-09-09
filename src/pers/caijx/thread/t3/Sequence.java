e593b268d53a56525e1d9c44e6a85412d9b46943
SmileCJX <1092925421@qq.com>
2018-09-09 15:28:39 +0800

    使用synchronized修饰静态方法和修饰代码块

--- src/pers/caijx/thread/t3/Sequence.java
+++ src/pers/caijx/thread/t3/Sequence.java
@@ -7,10 +7,36 @@ package pers.caijx.thread.t3;
  */
 public class Sequence {
 
-    private int value;
+    private static int value;
 
+    /**
+     * synchronized 放在普通方法上，内置锁就是当前类的实例
+     * @return
+     */
     public synchronized int getNext() {
         return value++;
+    }
+
+    /**
+     * 修饰代码块
+     * @return
+     */
+    public int method() {
+        synchronized (this) {
+            if (value > 0) {
+                return value;
+            } else {
+                return -1;
+            }
+        }
+    }
+
+    /**
+     * 修饰静态方法，内置锁是当前的Class字节码对象
+     * @return
+     */
+    public static synchronized int getPrevious() {
+        return value--;
     }
 
     public static void main(String[] args) {
